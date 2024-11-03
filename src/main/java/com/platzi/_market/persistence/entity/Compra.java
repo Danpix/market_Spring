5f80fedf0780cd8240aafb5c484d137329e5988d
Kevin Chan <kevin.chan1512@gmail.com>
2024-11-03 12:54:25 -0600

    Crecion_de_Entidades

---
+++ src/main/java/com/platzi/_market/persistence/entity/Compra.java
@@ -0,0 +1,68 @@
+package com.platzi._market.persistence.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "compras")
+public class Compra {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_compra;
+    private String id_cliente;
+    private LocalDateTime fecha;
+    private String medio_pago;
+    private String comentario;
+    private String estado;
+
+    public Integer getId_compra() {
+        return id_compra;
+    }
+
+    public void setId_compra(Integer id_compra) {
+        this.id_compra = id_compra;
+    }
+
+    public String getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(String id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getMedio_pago() {
+        return medio_pago;
+    }
+
+    public void setMedio_pago(String medio_pago) {
+        this.medio_pago = medio_pago;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+
+}
